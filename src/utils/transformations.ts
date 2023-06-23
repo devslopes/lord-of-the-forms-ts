8cf6d29c52af57a7c545aa8a2db9e4090865945f
jjhiggz <jonathan.higger@gmail.com>
2023-06-23 16:02:19 -0600

    done

--- src/utils/transformations.ts
+++ src/utils/transformations.ts
@@ -1,4 +1,9 @@
 export const capitalize = () => {
-    // todo build this function
+    // todo: build this function
     // `capitalize("jOn")` should output `"Jon"`
+}
+
+export const formatPhoneNumber = () => {
+    // todo: build this function
+    // `formatPhoneNumber("1234567")` should be `"12-34-56-7"`
 }
